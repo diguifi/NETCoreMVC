3b58f0e6706d9b1654fa094bc838ad819617c9b6
diguifi <lumia-diguifi@hotmail.com>
2019-04-09 21:38:23 -0300

    Add dbcontext

--- NETCoreMVC/Startup.cs
+++ NETCoreMVC/Startup.cs
@@ -6,8 +6,10 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using NETCoreMVC.Data;
 
 namespace NETCoreMVC
 {
@@ -30,6 +32,10 @@ namespace NETCoreMVC
                 options.MinimumSameSitePolicy = SameSiteMode.None;
             });
 
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("Default"))
+            );
+            
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
